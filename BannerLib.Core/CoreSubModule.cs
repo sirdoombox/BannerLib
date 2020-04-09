2fad652fbf8b09517418888acbf518f64f1345d7
Doombox <Aaron.j.king.95@gmail.com>
2020-04-09 11:56:50 +0100

    Loaded message only appears once now.

--- BannerLib.Core/CoreSubModule.cs
+++ BannerLib.Core/CoreSubModule.cs
@@ -6,8 +6,11 @@ namespace BannerLib.Core
 {
     public class CoreSubModule : MBSubModuleBase
     {
+        private bool m_isLoaded;
         protected override void OnBeforeInitialModuleScreenSetAsRoot()
         {
+            if (m_isLoaded) return;
+            m_isLoaded = true;
             InformationManager.DisplayMessage(new InformationMessage("Loaded BannerLib", Colors.Green));
         }
     }
